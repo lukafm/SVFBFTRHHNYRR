f1f471f78acdb7c4d5cef102fe876cd1a703ce5d
lukaf <lukaf@LAPTOP-326TET7C>
2024-03-10 18:12:11 +0100

    HOLA

---
+++ proyecto2/src/carpeta/EJERCIO1.java
@@ -0,0 +1,10 @@
+package carpeta;
+
+public class EJERCIO1 {
+
+	public static void main(String[] args) {
+		System.out.println("HOLA");
+
+	}
+
+}
---
+++ proyecto2/src/carpeta/ejercicio.java
@@ -0,0 +1,10 @@
+package carpeta;
+
+public class ejercicio {
+
+	public static void main(String[] args) {
+		System.out.println("mensaje 1");
+
+	}
+
+}
